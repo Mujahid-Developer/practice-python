d69beb82c82de885392af5cdd1f3cdef87eb7b8c
Mujahid-Developer <mujahidulislam575@gmail.com>
2021-01-07 02:48:52 +0600

    day1 learn python with Mosh Hamedani

---
+++ main.py
@@ -0,0 +1,30 @@
+# String
+my_name = "mujahidul islam Munna"
+print(len(my_name))
+print(my_name[0])
+print(my_name[-1])
+print(my_name[0:7])
+print(my_name[0:])
+print(my_name[0:7])
+print(my_name[:])
+# String Method
+print(my_name.upper())
+print(my_name.lower())
+print(my_name.title())
+print(my_name.strip())
+print(my_name.find("Mun"))
+print(my_name.replace("m", "j"))
+print("Mun" in my_name)
+print("mun" not in my_name)
+# Number
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10 % 3)
+print(10**3)
+
+x = 10
+x = x + 3
+x += 3
